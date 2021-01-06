6ff99cb3be760c73d7ae907aa85d3b53b9656830
Abdul Aziz <dev.abdaziz@outlook.com>
2021-01-06 11:35:59 +0700

    modify index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="main text-center flex flex-col items-center justify-center">
     <!-- <logo /> -->
     <h1 class="title">Nuxt — Tailwind — Netlify CMS</h1>
-    <h2 class="subtitle">Boilerplate</h2>
+    <h2 class="subtitle">Latihan</h2>
   </div>
 </template>
 
@@ -15,8 +15,8 @@ export default {
   // },
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     }
-  }
+  },
 }
 </script>
